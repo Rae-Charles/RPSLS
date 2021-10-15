e9532e87ceb6dd15b4b17400bd528e13930c48c9
Rachel Harris <rachelharris@MacBook-Air.local>
2021-10-15 18:34:19 -0500

    Adjusted while loops for function

--- rpsls.py
+++ rpsls.py
@@ -6,36 +6,36 @@ from typing import AsyncIterable
 display_greeting = print("Welcome to Rock, Paper, Scissors, Lizard, Spock!!!")
 
 
-answer = input("Would you like to play a game of RPSLS?")
-if answer == 'yes':
+question = input("Would you like to play a game of RPSLS?")
+while question != "yes":
+    question = input("Please select yes or exit the game. Enter yes to continue: ")
+if question == "yes":
     print("Here are the rules for this best 2 out of 3 game:", "\n" "1. Paper beats Rock", "\n" "2. Rock beats Scissors", "\n" "3. Paper covers Rock", "\n" "4. Rock crushes Lizard", "\n" "5. Lizard poisons Spock", "\n" "6. Spock smashes Scissors", "\n" "7. Scissors decapitates Lizard", "\n" "8. Lizard eats Paper", "\n" "9. Paper disproves Spock", "\n" "10. Spock vaporizes Rock", "\n" "Whichever player wins 2 out of 3 games first, wins!")
-else:
-    print("Please select yes or exit the game.")
-    # return to input
+        
 
 
 
 player_mode = input("If you would like to play against the computer please press 1, if you would like to play with another player please select 2!")
+while player_mode != "1" and player_mode != "2":
+    player_mode = input("Please make a selection from the list...")
 if player_mode == "1":
     print("You have selected to play against the computer!")
 elif player_mode == "2":
     print("You have choosen to play against another player!")
-else:    
-    print("Please make a selection from the list...")
-    # return to game_mode input
 
 
 
-player_name = input("Player please enter your name: ")
-    print("Let's get ready to rumble "player_name"!!)
+
+# player_name = input("Player please enter your name: ")
+#     print("Let's get ready to rumble "player_name"!!)
 
 
 
-player1_gesture = input("Player, please select your gesture:" , "\n" "Press 1 for Rock", "\n" "Press 2 for Paper", "\n" "Press 3 for Scissors", "\n" "Press 4 for Lizard", "\n" "Press 5 for Spock")
-    print("Thank you for your selection!")
+# player1_gesture = input("Player, please select your gesture:" , "\n" "Press 1 for Rock", "\n" "Press 2 for Paper", "\n" "Press 3 for Scissors", "\n" "Press 4 for Lizard", "\n" "Press 5 for Spock")
+#     print("Thank you for your selection!")
 
-player2_gesture = input("Player, please select your gesture:" , "\n" "Press 1 for Rock", "\n" "Press 2 for Paper", "\n" "Press 3 for Scissors", "\n" "Press 4 for Lizard", "\n" "Press 5 for Spock")
-    print("Thank you for your selection!")
+# player2_gesture = input("Player, please select your gesture:" , "\n" "Press 1 for Rock", "\n" "Press 2 for Paper", "\n" "Press 3 for Scissors", "\n" "Press 4 for Lizard", "\n" "Press 5 for Spock")
+#     print("Thank you for your selection!")
 
 
 
