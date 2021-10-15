38c449120856efc23e4d56876021596dac561841
Rachel Harris <rachelharris@MacBook-Air.local>
2021-10-15 17:45:02 -0500

    Added pllayer gesture, needs fine tuning, just a draft.

--- rpsls.py
+++ rpsls.py
@@ -10,8 +10,9 @@ answer = input("Would you like to play a game of RPSLS?")
 if answer == 'yes':
     print("Here are the rules for this best 2 out of 3 game:", "\n" "1. Paper beats Rock", "\n" "2. Rock beats Scissors", "\n" "3. Paper covers Rock", "\n" "4. Rock crushes Lizard", "\n" "5. Lizard poisons Spock", "\n" "6. Spock smashes Scissors", "\n" "7. Scissors decapitates Lizard", "\n" "8. Lizard eats Paper", "\n" "9. Paper disproves Spock", "\n" "10. Spock vaporizes Rock", "\n" "Whichever player wins 2 out of 3 games first, wins!")
 else:
-    pass
-    # return input
+    print("Please select yes or exit the game.")
+    # return to input
+
 
 
 player_mode = input("If you would like to play against the computer please press 1, if you would like to play with another player please select 2!")
@@ -20,20 +21,26 @@ if player_mode == "1":
 elif player_mode == "2":
     print("You have choosen to play against another player!")
 else:    
-    pass
-    # print("Please make a selection from the list...")
+    print("Please make a selection from the list...")
     # return to game_mode input
 
 
 
+player_name = input("Player please enter your name: ")
+    print("Let's get ready to rumble "player_name"!!)
 
-# 1. welcome
-# 2. display the rules (how to play)
-# 3. info about game format (best of 3)
-# 4. choose game mode (pvp or pvAI)
-# 5. ask for player names
-# 6. p1 selects gesture
-# 7. p2 selects gesture
+
+
+player1_gesture = input("Player, please select your gesture:" , "\n" "Press 1 for Rock", "\n" "Press 2 for Paper", "\n" "Press 3 for Scissors", "\n" "Press 4 for Lizard", "\n" "Press 5 for Spock")
+    print("Thank you for your selection!")
+
+player2_gesture = input("Player, please select your gesture:" , "\n" "Press 1 for Rock", "\n" "Press 2 for Paper", "\n" "Press 3 for Scissors", "\n" "Press 4 for Lizard", "\n" "Press 5 for Spock")
+    print("Thank you for your selection!")
+
+
+
+
+
 # 8. compare our two gestures
 #    -same thing? no points!
 # 9. declare winner and give them 1 point
@@ -45,12 +52,4 @@ else:
 # Classes:
 #    Player-human
 #         AI
-# Game (game logic here)
-
-
-
-
-
-
-
-# Spock vaporizes Rock        
+# Game (game logic here)   
