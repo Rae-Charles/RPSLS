6abd51de52dbe27cae9c319c2948f364fcfdd8a9
Rachel Harris <rachelharris@MacBook-Air.local>
2021-10-15 15:43:27 -0500

    Added instructions for developer to structure program.

---
+++ rpsls.py
@@ -0,0 +1,44 @@
+from ast import get_source_segment
+import random
+from typing import AsyncIterable
+
+display_greeting = print("Welcome to Rock, Paper, Scissors, Lizard, Spock!!!")
+
+ready_to_play = input("Would you like to play a game of RPSLS?")
+    answer = yes
+        if answer = True
+
+
+
+
+
+# 1. welcome
+# 2. display the rules (how to play)
+# 3. info about game format (best of 3)
+# 4. choose game mode (pvp or pvAI)
+# 5. ask for player names
+# 6. p1 selects gesture
+# 7. p2 selects gesture
+# 8. compare our two gestures
+#    -same thing? no points!
+# 9. declare winner and give them 1 point
+# 10. check scores to see if someone has 2 pts (best 2 of 3)
+# 11. in no winner, loop back to step 6 and go again
+# 12. if there is a winner, congrats!end game
+
+
+# Classes:
+#    Player-human
+#         AI
+# Game (game logic here)
+
+# -Rock crushes Scissors 
+# -Scissors cuts Paper 
+# -Paper covers Rock 
+# Rock crushes Lizard 
+# Lizard poisons Spock 
+# Spock smashes Scissors 
+# Scissors decapitates Lizard
+# Lizard eats Paper 
+# Paper disproves Spock 
+# Spock vaporizes Rock        
