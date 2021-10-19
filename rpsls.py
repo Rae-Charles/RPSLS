85aded3886d2b23cbf46319c3ba32db6ed78364a
Rachel Harris <rachelharris@MacBook-Air.local>
2021-10-19 16:29:38 -0500

    Finalized and tested project, updated method in welcome message function.

--- rpsls.py
+++ rpsls.py
@@ -12,6 +12,8 @@ while question != "yes":
 if question == "yes":
     print("Here are the rules for this best 2 out of 3 game:", "\n" "1. Paper beats Rock", "\n" "2. Rock beats Scissors", "\n" "3. Paper covers Rock", "\n" "4. Rock crushes Lizard", "\n" "5. Lizard poisons Spock", "\n" "6. Spock smashes Scissors", "\n" "7. Scissors decapitates Lizard", "\n" "8. Lizard eats Paper", "\n" "9. Paper disproves Spock", "\n" "10. Spock vaporizes Rock", "\n" "Whichever player wins 2 out of 3 games first, wins!")
         
+
+
 def choose_mode():
     player_mode = input("If you would like to play against the computer please enter 1, if you would like to play with another player please enter 2! ")
     while player_mode != "1" and player_mode != "2":
@@ -24,6 +26,8 @@ def choose_mode():
         return 2
 
 print(f"Let's get ready to rumble {player_name}!!")
+
+
 
 
 # game mode 1 :user vs computer:
@@ -42,13 +46,14 @@ def play():
     return (-1, user, computer)
 
 
+
 def is_win(player, opponent):
     if (player == 'ro' and opponent == 'sc') or (player == 'sc' and opponent == 'pa') or (player == 'pa' and opponent == 'ro') or (player == 'li' and opponent == 'pa') or (player == 'li' and opponent == 'sp') or (player == 'ro' and opponent == 'li') or (player == 'sp' and opponent == 'sc') or (player == 'sp' and opponent == 'ro'):
         return True
     return False    
 
 
-# test if curly brackets will work in this function or need parenthesis
+
 def play_best_of(z):
     
     player_wins = 0
@@ -58,15 +63,15 @@ def play_best_of(z):
         result, user, computer = play()
 
         if result == 0:
-            print("It is a tie. You and the computer have both chosen {}. \n.format{user}")
+            print("It is a tie. You and the computer have both chosen {}.\n".format(user))
 
         elif result == 1:
             player_wins += 1    
-            print("Great job, you won! You chose {} and the computer chose {}. \n.format{user}{computer}")
+            print("Great job, you won! You chose {} and the computer chose {}.\n".format(user, computer))
 
         else:
             computer_wins += 1
-            print("You lost, what a shame! You chose {} and the computer trumped you with {}! \n.format{user}{computer}")
+            print("You lost, what a shame! You chose {} and the computer trumped you with {}!\n".format(user, computer))
 
     if player_wins > computer_wins:
         print("You have won the best of {} games! Congratulations!".format(z))
@@ -106,10 +111,12 @@ def play_user(p1, p2):
     return (-1, user1, user2)
 
 
+
 def is_win_user(user1, user2):
     if (user1 == 'ro' and user2 == 'sc') or (user1 == 'sc' and user2  == 'pa') or (user1  == 'pa' and user2  == 'ro') or (user1  == 'li' and user2  == 'pa') or (user1  == 'li' and user2  == 'sp') or (user1  == 'ro' and user2  == 'li') or (user1 == 'sp' and user2  == 'sc') or (user1  == 'sp' and user2  == 'ro') or (user1  == 'pa' and user2 == 'sp'):
         return True
     return False
+
 
 
 def play_best_of_three(z, user1_name, user2_name):
